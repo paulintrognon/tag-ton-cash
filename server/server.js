b83759db24fbc638a2490fa920356620d87d6667
Paulin Trognon <paulin.trognon@gmail.com>
2017-04-17 20:27:15 +0200

    add express

---
+++ server/server.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const express = require('express');
+const path = require('path');
+const http = require('http');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, '../dist')));
+
+const api = require('./routes/api.js');
+app.use('/api', api);
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../dist/index.html'));
+});
+
+const port = process.env.PORT || '3000';
+app.set('port', port);
+
+const server = http.createServer(app);
+
+server.listen(port, () => {
+  const address = server.address();
+  console.log(`API up and running on ${address.address}:${address.port}`);
+});
